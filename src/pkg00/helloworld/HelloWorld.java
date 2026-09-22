291cc62c5cd19b96f70378059a17c27db4f65b46
arohan <arohan@350028R-F115X11.lise.kreizenn.bzh>
2026-09-22 12:46:32 +0200

    modification test

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloworld;
-
+import java.util.Scanner;
 /**
  *
  * @author arohan
@@ -14,8 +14,18 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*Programme 2 lignes
         System.out.println("Hello");
-        System.out.println("World");
+        System.out.println("World");*/
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int y;
+        System.out.print("Saisir x :");
+        x = sc.nextInt();
+        System.out.print("Saisir y :");
+        y = sc.nextInt();
+        System.out.print("Avant permutation : x="+x+" y="+y);
+        System.out.print("Apres permutation : x="+y+" y="+x);
     }
     
 }
